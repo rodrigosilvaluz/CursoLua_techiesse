96a8e9cc48f62916a93b06ac542216c8d97c0476
Eric Chiesse <techiesse@gmail.com>
2019-03-27 00:43:50 -0300

    Setup pre aula. Removidas funções ainda não gravadas. Adicionada a opção de passar a data por linha de comando.

--- Monero/src/monero.lua
+++ Monero/src/monero.lua
@@ -12,10 +12,13 @@ function main(...)
     local args = {...}
     if args[1] == "busca" then
         local countryName = args[2]
+        local day = args[3] or os.date("%Y%m%d")
 
-        local inputFileName = COIN_DIR .. "\\" .. "M" .. os.date("%Y%m%d") .. ".csv"
+        local inputFileName = COIN_DIR .. "\\" .. "M" .. day .. ".csv"
         local contents = readTextFromFile(inputFileName)
         if contents == nil then
+            print(string.format("Arquivo nao encontrado: %s", inputFileName))
+            os.exit()
             -- Baixar Arquivo do BC:
         end
         local coins = coinTable(contents)
