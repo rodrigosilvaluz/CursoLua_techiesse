a38acc59a1ad07a445dbf6f9d95f8ac93231a719
Eric Chiesse <techiesse@gmail.com>
2019-08-01 01:31:39 -0300

    Criado arquivo exclusivo para comandos.

---
+++ Monero/src/commands.lua
@@ -0,0 +1,61 @@
+local commands = {}
+
+function commands.busca(args)
+    local countryName = args[1]
+    local day = args[2] or os.date("%Y%m%d")
+
+    local inputFileName = COIN_DIR .. "\\" .. genCoinTableFileName(day)
+    local contents = readTextFromFile(inputFileName)
+    if contents == nil then
+        -- Baixar Arquivo do BC:
+        downloadCoinTable(day, COIN_DIR)
+        contents = readTextFromFile(inputFileName)
+    end
+    local coins = readCoins(contents)
+    local filteredCoins = filterCoinsByCountry(coins, string.upper(countryName))
+    local validCoins = filterValidCoins(filteredCoins)
+    local code = validCoins[1].coinCode
+
+    print("Codigo: " .. code .. " - Simbolo: " .. validCoins[1].symbol)
+end
+
+
+function commands.converter(args)
+    local amount = tonumber(args[1])
+    local srcCoinSymbol = string.upper(args[2])
+    local destCoinSymbol = string.upper(args[3])
+    local dateStr = args[4] --or os.date("%Y%m%d")
+
+    -- Tentar abrir o arquivo de cotações
+    local inputFileName = COIN_DIR .. "\\" .. genQuotationFileName(dateStr)
+    local contents = readTextFromFile(inputFileName)
+    -- Se não existir baixar um novo
+    if contents == nil then
+        -- Baixar Arquivo do BC:
+        downloadQuotationTable(day, COIN_DIR)
+        contents = readTextFromFile(inputFileName)
+    end
+
+    -- Carregar tabela de cotações
+    local quotations = readQuotationTable(contents)
+
+    -- Obter cotações da moeda de origem e da moeda de destino (usar o simbolo)
+    local srcQuotation = quotations[srcCoinSymbol]
+    if srcQuotation == nil then
+        print('Cotacao de origem nao encontrada. Simbolo: ' .. srcCoinSymbol)
+        os.exit(1)
+    end
+    local destQuotation = quotations[destCoinSymbol]
+    if destQuotation == nil then
+        print('Cotacao de destino nao encontrada. Simbolo: ' .. destCoinSymbol)
+        os.exit(1)
+    end
+
+    -- Realizar a conversão
+    local convertedAmount = convert(amount, srcQuotation, destQuotation)
+
+    -- Exibir o resultado
+    print(string.format('%s %s = %s %s', amount, srcQuotation.coinSymbol, convertedAmount, destQuotation.coinSymbol))
+end
+
+return commands
--- Monero/src/monero.lua
+++ Monero/src/monero.lua
@@ -5,67 +5,20 @@ require "ext_libs.ioUtils"
 require "bc"
 require "util"
 
-COIN_DIR = "moedas"
+local cmd = require "commands"
 
+COIN_DIR = "moedas"
 
 function main(...)
     local args = {...}
     local commandName = args[1]
-    if commandName == "busca" then
-        local countryName = args[2]
-        local day = args[3] or os.date("%Y%m%d")
 
-        local inputFileName = COIN_DIR .. "\\" .. genCoinTableFileName(day)
-        local contents = readTextFromFile(inputFileName)
-        if contents == nil then
-            -- Baixar Arquivo do BC:
-            downloadCoinTable(day, COIN_DIR)
-            contents = readTextFromFile(inputFileName)
-        end
-        local coins = readCoins(contents)
-        local filteredCoins = filterCoinsByCountry(coins, string.upper(countryName))
-        local validCoins = filterValidCoins(filteredCoins)
-        local code = validCoins[1].coinCode
-
-        print("Codigo: " .. code .. " - Simbolo: " .. validCoins[1].symbol)
-
-
-    elseif commandName == "converter" then
-        local amount = tonumber(args[2])
-        local srcCoinSymbol = string.upper(args[3])
-        local destCoinSymbol = string.upper(args[4])
-        local dateStr = args[5] --or os.date("%Y%m%d")
-
-        -- Tentar abrir o arquivo de cotações
-        local inputFileName = COIN_DIR .. "\\" .. genQuotationFileName(dateStr)
-        local contents = readTextFromFile(inputFileName)
-        -- Se não existir baixar um novo
-        if contents == nil then
-            -- Baixar Arquivo do BC:
-            downloadQuotationTable(day, COIN_DIR)
-            contents = readTextFromFile(inputFileName)
-        end
-
-        -- Carregar tabela de cotações
-        local quotations = readQuotationTable(contents)
-
-        -- Obter cotações da moeda de origem e da moeda de destino (usar o simbolo)
-        local srcQuotation = quotations[srcCoinSymbol]
-        if srcQuotation == nil then
-            print('Cotacao de origem nao encontrada. Simbolo: ' .. srcCoinSymbol)
-            os.exit(1)
-        end
-        local destQuotation = quotations[destCoinSymbol]
-        if destQuotation == nil then
-            print('Cotacao de destino nao encontrada. Simbolo: ' .. destCoinSymbol)
-            os.exit(1)
-        end
-
-        -- Realizar a conversão
-        local convertedAmount = convert(amount, srcQuotation, destQuotation)
-
-        -- Exibir o resultado
-        print(string.format('%s %s = %s %s', amount, srcQuotation.coinSymbol, convertedAmount, destQuotation.coinSymbol))
+    args = {select(2, ...)}
+    if cmd[commandName] ~= nil then
+        cmd[commandName](args)
+    else
+        print('Comando nao encontrado: ' .. commandName)
+        os.exit(1)
     end
 end
 
